6cd3cb0717db3becdbf15e1f062914f20e657fd1
gregcodesaway <151475816+gregcodesaway@users.noreply.github.com>
2025-04-15 18:05:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -74,5 +75,8 @@ def get_barbers():
 
     return jsonify(barbers)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
