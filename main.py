03ddc717bf132248eacde3a1f85a3628cd635987
gregcodesaway <151475816+gregcodesaway@users.noreply.github.com>
2025-04-15 16:32:38 +0200

    Initial backend commit from Replit

--- main.py
+++ main.py
@@ -1,11 +1,59 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+barbers = [
+    {
+        "id": 1,
+        "name": "BRUHN Barbershop",
+        "address": "Albert Cuypstraat 156E, 1073 BK Amsterdam",
+        "services": ["Haircut", "Beard Trim", "Hot Towel Shave", "Styling", "Fade"],
+        "rating": 4.8,
+        "speaks_english": True,
+        "opening_hours": "Mon: 10:00–19:00, Tue: 12:00–20:00, Wed–Fri: 08:20–21:00, Sat: 09:00–19:00, Sun: 12:00–20:00",
+        "contact": "https://www.bruhn.nl",
+        "image_url": "https://cdn.prod.website-files.com/642843be0da449a9eaf4accd/651c1dfdaaca2da0c4742ec0_IMG_0392-p-500.webp"
+    },
+    {
+        "id": 2,
+        "name": "Prime Barbershop",
+        "address": "Nieuwendijk 85, 1012 MC Amsterdam",
+        "services": ["Haircut", "Beard Trim", "Hair Coloring","Fade"],
+        "rating": 4.9,
+        "speaks_english": True,
+        "opening_hours": "Mon–Sun: 09:30–20:00",
+        "contact": "https://primebarbershop.nl",
+        "image_url": "https://primebarbershop.nl/wp-content/uploads/2023/09/cropped-Prime-Barber-shop-01.png"
+    },
+    {
+        "id": 3,
+        "name": "City Center Barbershop",
+        "address": "Stationsplein 41M, 1012 AB Amsterdam",
+        "services": ["Haircut", "Beard Trim", "Hot Towel Shave","Fade"],
+        "rating": 4.6,
+        "speaks_english": True,
+        "opening_hours": "Mon–Fri: 08:00–21:00, Sat: 09:00–20:00, Sun: 10:00–20:00",
+        "contact": "https://citycenterbarbershop.nl",
+        "image_url": "https://citycenterbarbershop.nl/wp-content/uploads/2024/03/citycenterbarber-white-1.webp"
+    },
+    {
+        "id": 4,
+        "name": "Cut Throat Amsterdam",
+        "address": "Beursplein 5, 1012 JW Amsterdam",
+        "services": ["Haircut", "Beard Trim", "Hot Towel Shave", "Facial","Fade"],
+        "rating": 4.7,
+        "speaks_english": True,
+        "opening_hours": "Mon: 09:00–19:00, Tue: 09:00–21:00, Wed–Thu: 09:00–20:00, Fri: 08:00–19:00, Sat: 08:00–18:00, Sun: 11:00–18:00",
+        "contact": "https://cutthroatbarber.nl",
+        "image_url": "http://cutthroatbarber.nl/wp-content/uploads/2022/05/cutthroat-logo.png"
+    }
+]
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+@app.route("/barbers", methods=["GET"])
+def get_barbers():
+    return jsonify(barbers)
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
