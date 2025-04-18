7196cc4b83125c9bab2e116464b6079f94cd5ee3
gregcodesaway <151475816+gregcodesaway@users.noreply.github.com>
2025-04-18 21:51:43 +0200

    Student Discount update

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ barbers = [
         "rating": 4.7,
         "speaks_english": True,
         "walk_ins": False,
+        "student_discount": False,
         "opening_hours": "Mon: 10:00–19:00, Tue: 12:00–20:00, Wed–Fri: 08:20–21:00, Sat: 09:00–19:00, Sun: 12:00–20:00",
         "contact": "https://www.bruhn.nl",
         "image_url": "https://cdn.prod.website-files.com/642843be0da449a9eaf4accd/651c1dfdaaca2da0c4742ec0_IMG_0392-p-500.webp"
@@ -27,6 +28,7 @@ barbers = [
         "rating": 4.9,
         "speaks_english": True,
         "walk_ins": True,
+        "student_discount": False,
         "opening_hours": "Mon–Sun: 09:30–20:00",
         "contact": "https://primebarbershop.nl",
         "image_url": "https://primebarbershop.nl/wp-content/uploads/2023/09/cropped-Prime-Barber-shop-01.png"
@@ -39,6 +41,7 @@ barbers = [
         "rating": 4.9,
         "speaks_english": True,
         "walk_ins": False,
+        "student_discount": False,
         "opening_hours": "Mon–Fri: 08:00–21:00, Sat: 09:00–20:00, Sun: 10:00–20:00",
         "contact": "https://citycenterbarbershop.nl",
         "image_url": "https://citycenterbarbershop.nl/wp-content/uploads/2024/03/citycenterbarber-white-1.webp"
@@ -51,6 +54,7 @@ barbers = [
         "rating": 4.6,
         "speaks_english": True,
         "walk_ins": False,
+        "student_discount": True,
         "opening_hours": "Mon: 09:00–19:00, Tue: 09:00–21:00, Wed–Thu: 09:00–20:00, Fri: 08:00–19:00, Sat: 08:00–18:00, Sun: 11:00–18:00",
         "contact": "https://cutthroatbarber.nl",
         "image_url": "http://cutthroatbarber.nl/wp-content/uploads/2022/05/cutthroat-logo.png"
@@ -63,6 +67,7 @@ barbers = [
         "rating": 4.8,
         "speaks_english": True,
         "walk_ins": False,
+        "student_discount": True,
         "opening_hours": "Mon: ClOSED, Tue: 10:00–19:00, Wed–Thu: 11:00–20:00, Fri: 10:00–18:30, Sat: 10:00–18:30, Sun: CLOSED",
         "contact": "https://roguerazor.nl/",
         "image_url": "https://impro.usercontent.one/appid/oneComWsb/domain/roguerazor.nl/media/roguerazor.nl/onewebmedia/RR-logo-new___serialized1.png?etag=%229248-621e1e62%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=564%2B461&extract=25%2B0%2B538%2B460"
@@ -75,10 +80,24 @@ barbers = [
         "rating": 4.9,
         "speaks_english": True,
         "walk_ins": False,
+        "student_discount": False,
         "opening_hours": "Mon-Fri: 10:00–18:30, Sat: 09:30–18:30, Sun: CLOSED",
         "contact": "https://mrelias.nl/",
         "image_url": "https://mrelias.nl/wp-content/uploads/2020/11/logo.png"
-    }
+    },
+    {
+  "id": 7,
+  "name": "HIS Amsterdam",
+  "address": "Utrechtsedwarsstraat 86, 1017 WH Amsterdam",
+  "services": ["Beard Trim","Haircut","Hot Towel","Facial","Beard Colouring","Eye Mask","Face Mask","Peel Mask","Body Wax"],
+  "rating": 4.5,
+  "speaks_english": true,
+  "walk_ins": false,
+  "student_discount": False,
+  "opening_hours": "Mon: Closed, Tues: 10:00–19:00, Weds: 10:00–19:00, Thurs: 10:00–21:00, Fri: 10:00–19:00, Sat: 10:00–19:00, Sun: 12:00–19:00",
+  "contact": "https://www.hisamsterdam.nl",
+  "image_url": "https://www.hisamsterdam.nl/wp-content/uploads/2023/02/his-amsterdam-logo-new-02.svg"
+}
 ]
 
 # Stronger fuzzy match logic
